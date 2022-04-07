581872fe19d5d182f297bb8ba8f6a43e21e34196
MICHAEL JAHN <michael.jm.jahn@daimler.com>
2022-04-07 09:50:05 +0200

    Add aliases for vim, which. Add winget directories for vim, git.

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -1,1 +1,4 @@
 oh-my-posh --init --shell pwsh --config $HOME\microverse-power-modified.omp.json | Invoke-Expression
+New-Alias which get-command
+New-Alias vi vim
+$Env:PATH += ";C:\Program Files\Scripts;C:\Users\jahmich\AppData\Local\Programs\Git\bin;C:\Users\jahmich\AppData\Local\Programs\Git\usr\bin"
