b1ab8a319c9c0dbd8b57c2e5a1af8c8116a5dc80
Michael Jahn <47208217+MichaelJahn2@users.noreply.github.com>
2022-10-03 12:21:13 +0200

    modify path variable, add vimrc

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -1,4 +1,4 @@
 oh-my-posh --init --shell pwsh --config $HOME\microverse-power-modified.omp.json | Invoke-Expression
 New-Alias which get-command
 New-Alias vi vim
-$Env:PATH += ";C:\Program Files\Scripts;C:\Users\jahmich\AppData\Local\Programs\Git\bin;C:\Users\jahmich\AppData\Local\Programs\Git\usr\bin"
+$Env:PATH += ";C:\Program Files\Scripts;C:\Users\jahmich\AppData\Local\Programs\Git\bin;C:\Users\jahmich\AppData\Local\Programs\Git\usr\bin;C:\Program Files (x86)\GnuWin32\bin"
