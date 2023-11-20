f4d6e2f69daf5a3158881970ae05cd4cddec27f8
Michael Jahn <47208217+MichaelJahn2@users.noreply.github.com>
2023-11-20 13:23:51 +0100

    modify readline behaviour to be similar to bash

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -1,4 +1,6 @@
 oh-my-posh --init --shell pwsh --config $HOME\microverse-power-modified.omp.json | Invoke-Expression
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 New-Alias which get-command
 New-Alias vi vim
 $Env:PATH += ";C:\Program Files\Scripts;C:\Users\jahmich\AppData\Local\Programs\Git\bin;C:\Users\jahmich\AppData\Local\Programs\Git\usr\bin;C:\Program Files (x86)\GnuWin32\bin"
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
